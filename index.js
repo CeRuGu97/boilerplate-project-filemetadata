8acb4f53abf03a58a86dd23f1e2c99f0bdb83c24
CeRuGu97 <cesarchivas72@gmail.com>
2025-04-28 22:16:06 -0600

    filemetadata

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer  = require('multer')
+const upload = multer({ storage: multer.memoryStorage() });
 
 var app = express();
 
@@ -10,6 +12,21 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.post('/api/fileanalyse', upload.single('archivo'), function (req, res) {
+  const archivo = req.file;
+
+  if (!archivo) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+  const fileInfo = {
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  };
+  res.json(fileInfo);
+});
+
 
 
 
